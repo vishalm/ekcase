ca94cbdec4d11302bdb49098d7e8349df4737359
Vishal <vishal.mishra@dnata.com>
2020-01-25 11:19:44 +0400

    Updated the slave packer image

--- jenkins/packer-ami/jenkins/slave/scripts/setup.sh
+++ jenkins/packer-ami/jenkins/slave/scripts/setup.sh
@@ -2,7 +2,13 @@
 echo "Install Docker"
 sudo yum update -y
 sudo yum -y install docker
-
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+sudo yum install epel-release -y
+sudo yum install jq -y
+jq --version
 
 echo "Install Java"
 sudo yum -y install java-1.8.0-openjdk-devel
